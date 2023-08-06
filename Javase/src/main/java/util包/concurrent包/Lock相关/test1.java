e75fc28ecf065b002b27c9c5f196ae591b18a46f
lupe <766550111@qq.com>
2023-08-07 04:44:30 +0800

    p

--- Javase/src/main/java/util包/concurrent包/Lock相关/test1.java
+++
@@ -1,5 +0,0 @@
-package util包.concurrent包.Lock相关;
-
-public class test1 {
-//给我写一个随机生成1-100的数组，数组长度为200
-}
--- Javase/src/main/java/特性包/函数式接口/functionalInterface.java
+++
@@ -1,5 +0,0 @@
-package 特性包.函数式接口;
-@FunctionalInterface
-public interface functionalInterface {
-    public void onlyFunction(Integer a, Integer b);
-}
